a4763320479e11a0eb7e327a98271160bed81027
hugohtz03 <hugoheintz03@gmail.com>
2024-01-08 07:48:05 +0100

    split en fonction

---
+++ air01.js
@@ -0,0 +1,36 @@
+// Fonction de découpe sans utiliser split
+function customSplit(arg, separateur) {
+
+    let mots = [];
+    let mot = '';
+    let separateurLength = separateur.length;
+
+    for (let i = 0; i < arg.length; i++) {
+        let sousChaine = arg.slice(i, i + separateurLength);
+
+        if (sousChaine === separateur) {
+            mots.push(mot);
+            mot = '';
+            i += separateurLength - 1; 
+        } else {
+            mot += arg[i];
+        }
+    }
+
+    mots.push(mot);
+
+    return mots;
+}
+
+// Parsing
+function getArgument() {
+    let arguments = process.argv.slice(2);
+    return arguments;
+}
+
+// Résolution
+let myArguments = getArgument();
+let result = customSplit(myArguments[0], myArguments[1]);
+
+// Affichage
+console.log(result.join(" "));
