89f85167a43a331e2ff553b04619b0d0174c1528
hugohtz03 <hugoheintz03@gmail.com>
2024-01-10 08:49:41 +0100

    un seul a la fois

---
+++ air04.js
@@ -0,0 +1,22 @@
+function oneToOne(arg) {
+    let newString = "";
+    for (let i = 0; i < arg.length; i++) {
+        if (arg[i] !== arg[i + 1]) {
+            newString += arg[i];
+        }
+    }
+    return newString;
+}
+
+// Gestion d'erreur / Parsing
+function getArgument() {
+    let argument = process.argv.slice(2).join(" "); 
+    return argument;
+}
+
+// Résolution
+let myArgument = getArgument();
+let result = oneToOne(myArgument);
+
+// Affichage
+console.log(result);
