77974193d2abafb330d3001e802b48587b4e481f
hugohtz03 <hugoheintz03@gmail.com>
2024-01-27 18:04:37 +0100

    insertion dans un tableau trié

---
+++ air07.js
@@ -0,0 +1,40 @@
+// Fonction utilisée
+function IsSorted(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] > array[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function InsertingIntoASortedArray(array, newElement) {
+  const isSorted = IsSorted(array);
+  
+  if (isSorted === true) {
+    let i = 0;
+    while (i < array.length && array[i] < newElement) {
+      i++;
+    }
+
+    array.splice(i, 0, newElement);
+
+    console.log(array);
+  } else {
+    array.sort((a, b) => a - b);
+    array.splice(i, 0, newElement);
+    console.log(array);
+  }
+}
+
+// Parsing
+function getArgument() {
+  let arguments = process.argv.slice(2);
+  return arguments;
+}
+
+// Résolution
+let myArguments = getArgument();
+let result = InsertingIntoASortedArray(myArguments.slice(0, -1).map(Number), Number(myArguments[myArguments.length - 1]));
+// Affichage
+console.log(result);
