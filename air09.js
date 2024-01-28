2f6f5daa5cd0d2296f0b031b5fa0f5d96e3eed68
hugohtz03 <hugoheintz03@gmail.com>
2024-01-28 15:55:03 +0100

    rotation vers la gauche

---
+++ air09.js
@@ -0,0 +1,34 @@
+// Fonction utilisée
+function ma_rotation(array) {
+    gestionErreurs(array);
+
+    const firstElement = array.shift();
+    array.push(firstElement);
+
+    return array;
+}
+
+// Gestion erreur 
+function gestionErreurs(array) {
+    if (!Array.isArray(array)) {
+        console.error("Erreur : L'argument doit être un tableau.");
+        process.exit(1);
+    }
+
+    if (array.length < 1) {
+        console.error("Erreur : Le tableau doit avoir au moins un élément.");
+        process.exit(1);
+    }
+}
+
+// Parsing
+function getArgument() {
+    return process.argv.slice(2);
+}
+
+// Résolution
+let myArguments = getArgument();
+let result = ma_rotation(myArguments);
+
+// Affichage du résultat
+console.log(result.join(', '));
