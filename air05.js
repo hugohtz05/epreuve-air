b9d55b4a9016096eba765c0f5f2f2a6400439596
hugohtz03 <hugoheintz03@gmail.com>
2024-02-14 14:17:52 +0100

    sur chacun d'entre eux

--- air05.js
+++ air05.js
@@ -1,21 +1,41 @@
+//fonction utilisée
 function onEach(arg, separateur) {
+
+    let result = [];
+    
+    let operator = (separateur[0] === '-') ? -1 : 1;
+    let operand = parseInt(separateur);
+
     for (let i = 0; i < arg.length; i++) {
-        let args = parseInt(arg[i]);
-        let sep = parseInt(separateur);
-        let resultat = args + sep;
-        console.log(resultat);
+        let currentNumber = parseInt(arg[i]);
+
+        if (isNaN(currentNumber)) {
+            console.error("Erreur : Argument non numérique.");
+            process.exit(1);
+        }
+
+        let resultNumber = currentNumber + (operator * operand);
+        result.push(resultNumber);
     }
+    
+    return result;
 }
 
-// Gestion d'erreur / Parsing
+// Parsing
 function getArgument() {
     let argument = process.argv.slice(2);
     return argument;
 }
 
 // Résolution
-let myArgument = getArgument();
-let result = onEach(myArgument.slice(0, -1), myArgument[myArgument.length - 1]);
+function getStringConcatened() {
+    const myArguments = getArgument();
+    const stringArray = myArguments.slice(0, -1);
+    const separator = myArguments[myArguments.length - 1];
+    const result = onEach(stringArray, separator);
+
+    return result;
+}
 
 // Affichage
-console.log(result);
+console.log(getStringConcatened().join(" "));
